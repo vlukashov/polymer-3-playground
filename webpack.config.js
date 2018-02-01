dda321a6a71a0c1b5b27247b9df7fef3fa14c6e8
Viktor Lukashov <viktor@vaadin.com>
2018-02-01 17:05:24 +0200

    add clean-webpack-plugin
    Cleans the output directory before the build starts to ensure there are no left-over files from earlier builds.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const OUTPUT_FOLDER = 'build';
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, OUTPUT_FOLDER)
   },
   plugins: [
+    new CleanWebpackPlugin([OUTPUT_FOLDER]),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     })
